9cf781738762e29212852d4ec981ce992c750994
Massoumeh_Mahaei <massoumeh.mahaei@syntax.com>
2022-04-24 14:16:04 -0400

    Massoumeh's assignment, for loop

---
+++ src/com/keep/calm/enjoy/java/c/loop/and/array/massi/assignment/LoopClass.java
@@ -0,0 +1,9 @@
+package com.keep.calm.enjoy.java.c.loop.and.array.massi.assignment;
+
+public class LoopClass {
+    public static void main(String[] args) {
+        for (int i=0;i<15;i++){
+            System.out.println("i: "+ i);
+        }
+    }
+}
