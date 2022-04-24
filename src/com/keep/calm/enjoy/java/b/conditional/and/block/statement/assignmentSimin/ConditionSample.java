b112a68789bacbae5d4ffec4012870d25e60b418
siminbigdeli <siminbigdelii@gmail.com>
2022-04-23 23:29:01 -0400

    my first assignment for conditions in JAVA...

---
+++ src/com/keep/calm/enjoy/java/b/conditional/and/block/statement/assignmentSimin/ConditionSample.java
@@ -0,0 +1,13 @@
+package com.keep.calm.enjoy.java.b.conditional.and.block.statement.assignmentSimin;
+
+public class ConditionSample {
+    public static void main(String[] args) {
+        int time = 10;
+        if (time < 10)
+            System.out.println("good morning...");
+        else if (time < 20 )
+            System.out.println("Good day...");
+        else
+            System.out.println("Good evening... ");
+    }
+}
