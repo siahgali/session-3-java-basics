8cccb7e2573f6039703c6de07445b06c72371f26
siminbigdeli <siminbigdelii@gmail.com>
2022-04-23 23:21:23 -0400

    my first assignment for casting in JAVA...

---
+++ src/com/keep/calm/enjoy/java/a/primitive/datattpe/assingment/simin/TypeCasteSample.java
@@ -0,0 +1,11 @@
+package com.keep.calm.enjoy.java.a.primitive.datattpe.assingment.simin;
+
+public class TypeCasteSample {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+
+    }
+}
