13114e311445955a7ff34f4c84a0e470f1738857
siminbigdeli <siminbigdelii@gmail.com>
2022-04-23 22:59:36 -0400

    my first assignment for data type :D

---
+++ src/com/keep/calm/enjoy/java/a/primitive/datattpe/assingment/simin/DatatypeSample.java
@@ -0,0 +1,16 @@
+package com.keep.calm.enjoy.java.a.primitive.datattpe.assingment.simin;
+
+public class DatatypeSample {
+    public static void main(String[] args) {
+        int myIntNum = 1;
+        float myFloatNum = 1.1f;
+        boolean myBoolean = true;
+        char myChar = 'S';
+        String myString = "Helooo";
+        System.out.println(myIntNum);
+        System.out.println(myFloatNum);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myString);
+    }
+}
