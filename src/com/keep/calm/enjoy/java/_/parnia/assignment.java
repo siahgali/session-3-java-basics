044f4e303d31570dcc13092816149d212c558891
Parnia <parniasadighi@gmail.com>
2022-04-24 21:12:32 -0400

    My First Switch Class

---
+++ src/com/keep/calm/enjoy/java/_/parnia/assignment.java
@@ -0,0 +1,11 @@
+package com.keep.calm.enjoy.java._.parnia;
+
+public class assignment {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
