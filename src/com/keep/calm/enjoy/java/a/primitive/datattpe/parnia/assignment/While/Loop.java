9d724b5004acafaa61ae56484dc0d6114d8a3714
Parnia <parniasadighi@gmail.com>
2022-04-24 22:33:31 -0400

    My First Switch Class

--- src/com/keep/calm/enjoy/java/a/primitive/datattpe/parnia/assignment/While/Loop.java
+++ src/com/keep/calm/enjoy/java/a/primitive/datattpe/parnia/assignment/While/Loop.java
@@ -5,7 +5,7 @@ import com.keep.calm.enjoy.java.a.primitive.datattpe.operator.precedence.Main;
 public class Loop {
 
     int i = 0;
-while (i < 5) {
+    while (i < 5) {
         System.out.println(i);
         i++;
     }
