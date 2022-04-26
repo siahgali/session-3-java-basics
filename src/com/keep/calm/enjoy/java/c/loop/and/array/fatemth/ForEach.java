8ec2071129028e633e8964203426c7f52e3931cc
fatemeh <fatemeh.ka84@gmail.com>
2022-04-26 09:53:09 -0400

    added Loop Through an Array

---
+++ src/com/keep/calm/enjoy/java/c/loop/and/array/fatemth/ForEach.java
@@ -0,0 +1,11 @@
+package com.keep.calm.enjoy.java.c.loop.and.array.fatemth;
+
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
