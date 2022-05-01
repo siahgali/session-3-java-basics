c156d12a867c3ff2446fa595330e91ec27e3a865
Parnia <parniasadighi@gmail.com>
2022-05-01 18:49:01 -0400

    my first assignment for conditions in JAVA...

---
+++ src/com/keep/calm/enjoy/java/c/loop/and/array/parnia/assignmentc/GradeProgram.java
@@ -0,0 +1,29 @@
+package com.keep.calm.enjoy.java.c.loop.and.array.parnia.assignmentc;
+import java.util.Scanner;
+public class GradeProgram {
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Grade of student");
+            float grade = scanner.nextFloat();
+
+            if(grade > 20){
+                System.out.println("w");
+            }else if(grade >= 17 && grade <= 20){
+                System.out.println("A");
+            }else if(grade >= 14 && grade < 17){
+                System.out.println("B");
+            }else if(grade >= 11 && grade < 14){
+                System.out.println("C");
+            }else if(grade >= 10 && grade < 11){
+                System.out.println("D");
+            }else if(grade >= 0  && grade  < 10) {
+                System.out.println("F");}
+            else{System.out.println("grade not valid");
+                }
+            }
+
+    }
+
+
+
