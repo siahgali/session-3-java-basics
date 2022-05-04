a1927bac801392c4d9d91dc091e695795a8751cc
farid <f.eslambolchi@gmail.com>
2022-05-03 21:26:21 -0400

    second assignment - last minute

---
+++ src/com/keep/calm/enjoy/java/c/loop/and/array/farid/assignment/second/SecondAssignment.java
@@ -0,0 +1,26 @@
+package com.keep.calm.enjoy.java.c.loop.and.array.farid.assignment.second;
+
+import java.util.Scanner;
+
+public class SecondAssignment {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter grade");
+        double grade = scan.nextDouble();
+
+        if (grade > 20) {
+            System.out.println("W");
+        } else if (grade > 17 && grade < 20) {
+            System.out.println("A");
+        } else if (grade > 14 && grade < 17) {
+            System.out.println("B");
+        } else if (grade > 11 && grade < 14) {
+            System.out.println("C");
+        } else if (grade > 10 && grade < 11) {
+            System.out.println("D");
+        } else if (grade > 0 && grade < 10) {
+            System.out.println("F");
+        }
+    }
+}
