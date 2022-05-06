7603977df54c585a69dbefc174a44e415d84077c
Zahra Arablou <arablouzahra@gmail.com>
2022-05-06 18:47:31 -0400

    Zahra assign 2 ( grade range )

--- src/com/keep/calm/enjoy/java/b/conditional/and/block/statement/zahra/assignment/second/SecondAssignment.java
+++ src/com/keep/calm/enjoy/java/b/conditional/and/block/statement/zahra/assignment/second/SecondAssignment.java
@@ -5,31 +5,29 @@ public class SecondAssignment {
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a grade ( > 0): ");
-
         int num = scan.nextInt();
         // Closing Scanner after the use
         scan.close();
-        char result = ' ' ;
+        char result = ' ';
 
-        if (num > 20 ) {
+        if (num > 20) {
             result = 'W';
-        } else if (num>17 && num<=20) {
+        } else if (num > 17 && num <= 20) {
             result = 'A';
+        } else if (num > 14 && num <= 17) {
+            result = 'B';
+        } else if (num > 11 && num <= 14) {
+            result = 'C';
+        } else if (num > 10 && num <= 11) {
+            result = 'D';
+        } else if (num > 0 && num <= 10) {
+            result = 'F';
+        } else {
+            System.out.println("your grade is not in the range  ");
         }
-         else if (num>14 && num<=17) { result='B';}
-          else if (num>11 && num<=14) { result='C'; }
-           else if (num>10 && num<=11) {result='D';}
-            else if (num>0 && num<=10) {result='F';}
-
-else {
-    System.out.println("your grade is not in the range  ");
-}
-if (result!=' ') {
-    System.out.println("your grade is in group: " + result);
-}
-}
-
-
+        if (result != ' ') {
+            System.out.println("your grade is in group: " + result);
+        }
     }
-
+}
 
